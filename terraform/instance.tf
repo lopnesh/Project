cd27ec7881b900f8532b5c5de033e1b963d67627
lopnesh <lopnesh@gmail.com>
2021-10-06 17:54:11 +0300

    first commit

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "aws" {
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-  region = "eu-central-1"
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
